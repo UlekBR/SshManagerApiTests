a26a7885f1c2146335d9f44a8515f7e892372e06
Paulo <127901575+UlekBR@users.noreply.github.com>
2024-07-03 16:37:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,17 +23,17 @@ if [ -d "/opt/SshManagerApi" ]; then
         # Determine a arquitetura e faça o download do arquivo correspondente
         if [[ $arch == "x86_64" || $arch == "amd64" || $arch == "x86_64h" ]]; then
             echo "Sistema baseado em x86_64 (64-bit Intel/AMD)"
-            curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/sshmanager"
+            curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/sshmanager"
         elif [[ $arch == "aarch64" || $arch == "arm64" || $arch == "armv8-a" ]]; then
             echo "Sistema baseado em arm64 (64-bit ARM)"
-            curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/sshmanagerarm"
+            curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/sshmanagerarm"
         else
             echo "Arquitetura não reconhecida: $arch"
             exit 1
         fi
 
         # Faça o download de outros scripts necessários
-        curl -o "/opt/SshManagerApi/menu.sh" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/menu.sh"
+        curl -o "/opt/SshManagerApi/menu.sh" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/menu.sh"
 
         # Dê permissão de execução aos scripts
         chmod +x /opt/SshManagerApi/sshmanagerapi
@@ -50,17 +50,17 @@ if [ -d "/opt/SshManagerApi" ]; then
         # Determine a arquitetura e faça o download do arquivo correspondente
         if [[ $arch == "x86_64" || $arch == "amd64" || $arch == "x86_64h" ]]; then
             echo "Sistema baseado em x86_64 (64-bit Intel/AMD)"
-            curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/sshmanager"
+            curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/sshmanager"
         elif [[ $arch == "aarch64" || $arch == "arm64" || $arch == "armv8-a" ]]; then
             echo "Sistema baseado em arm64 (64-bit ARM)"
-            curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/sshmanagerarm"
+            curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/sshmanagerarm"
         else
             echo "Arquitetura não reconhecida: $arch"
             exit 1
         fi
 
         # Faça o download de outros scripts necessários
-        curl -o "/opt/SshManagerApi/menu.sh" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/menu.sh"
+        curl -o "/opt/SshManagerApi/menu.sh" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/menu.sh"
 
         # Dê permissão de execução aos scripts
         chmod +x /opt/SshManagerApi/sshmanagerapi
@@ -78,17 +78,17 @@ else
     # Determine a arquitetura e faça o download do arquivo correspondente
     if [[ $arch == "x86_64" || $arch == "amd64" || $arch == "x86_64h" ]]; then
         echo "Sistema baseado em x86_64 (64-bit Intel/AMD)"
-        curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/sshmanager"
+        curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/sshmanager"
     elif [[ $arch == "aarch64" || $arch == "arm64" || $arch == "armv8-a" ]]; then
         echo "Sistema baseado em arm64 (64-bit ARM)"
-        curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/sshmanagerarm"
+        curl -o "/opt/SshManagerApi/sshmanagerapi" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/sshmanagerarm"
     else
         echo "Arquitetura não reconhecida: $arch"
         exit 1
     fi
 
     # Faça o download de outros scripts necessários
-    curl -o "/opt/SshManagerApi/menu.sh" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApi/main/menu.sh"
+    curl -o "/opt/SshManagerApi/menu.sh" -f "https://raw.githubusercontent.com/UlekBR/SshManagerApiTests/main/menu.sh"
 
     # Dê permissão de execução aos scripts
     chmod +x /opt/SshManagerApi/sshmanagerapi
